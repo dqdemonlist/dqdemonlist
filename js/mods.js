2e0a89c6b3bca803bce72c38fc91398793202a66
angyedz <megaanal24@gmail.com>
2025-12-21 14:31:48 +0300

    а

---
+++ js/mods.js
@@ -0,0 +1,19 @@
+// Для лаунчера, на сайт никак не влияет
+const mods = [
+    {
+        name: "Globed",
+        version: "v1.5.0",
+        description: "Мультиплеер для Geometry Dash.",
+        repo: "dankmeme01/globed2", // Владелец/Репозиторий
+        filename: "Globed.geode",
+        category: "mandatory" // "mandatory" (обязательные) или "other" (остальные)
+    },
+    {
+        name: "Node IDs",
+        version: "v1.2.0",
+        description: "Показывает ID объектов в редакторе.",
+        repo: "geode-sdk/node-ids",
+        filename: "NodeIds.geode",
+        category: "other"
+    }
+];
